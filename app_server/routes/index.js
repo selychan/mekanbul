4b334e72bb297b7a4a519a083fb299d3789f1167
selychan <sellay.14@hotmail.com>
2022-11-11 20:39:46 +0300

    odev4

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var ctrlMekanlar = require('../controllers/mekanlar');
-var ctrtDigerleri = require('../controllers/digerleri');
-
+var ctrlDigerleri = require('../controllers/digerleri');
 
 
 router.get('/', ctrlMekanlar.anaSayfa);
 router.get('/mekan', ctrlMekanlar.mekanBilgisi);
 router.get('/mekan/yorum/yeni', ctrlMekanlar.yorumEkle);
-router.get('/hakkinda', ctrtDigerleri.hakkinda);
+router.get('/hakkinda', ctrlMekanlar.mekanBilgisi);
+
 
 module.exports = router;
