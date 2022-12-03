25c7d9b7fc9fe4ecf1ef851bc328f47ae58efda0
selychan <sellay.14@hotmail.com>
2022-12-03 19:11:56 +0300

    odev7

---
+++ app_api/models/db.js
@@ -0,0 +1,35 @@
+var mongoose = require('mongoose');
+//var dbURI = "mongodb://127.0.0.1/mekanbul";
+var dbURI = "mongodb+srv://selay:selay00@mekanbul.u9mmvpp.mongodb.net/mekanbul?retryWrites=true&w=majority";
+require("./mekansema"); 
+
+
+function kapat(msg,callback){
+    mongoose.connection.close(function(){
+        console.log(msg);
+        callback();
+    });
+}
+
+process.on("SIGINT",function(){
+    kapat("Uygulama kapatıldı!",function(){
+        process.exit(0);
+    })
+})
+
+
+mongoose.connect(dbURI);
+mongoose.connection.on(
+    'connected', function(){
+        console.log(dbURI + " adresindeki veritabanına bağlanıldı!\n");
+    });
+    
+mongoose.connection.on(
+    'error', function(){
+        console.log(dbURI + "Bağlantı hatası!\n");
+    });
+    
+mongoose.connection.on(
+    'disconnected', function(){
+        console.log(dbURI + "Bağlantı koptu!\n");
+    });
